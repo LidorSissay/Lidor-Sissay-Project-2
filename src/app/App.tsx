a00f566a20a8b2d9eb9077ddd44d36854fae8427
LidorSissay <lidsis96w@gmail.com>
2026-05-04 14:54:57 +0300

    set redux store and coin slice

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from "react-router-dom"
 import Layout from "../layout/Layout"
+import { Provider as Redux } from "react-redux"
+import store from "../components/07-redux/store"
 
 const App = () => {
   return (
     <div className='App'>
       <BrowserRouter>
-        <Layout />
+        <Redux store={store}>
+          <Layout />
+        </Redux>
       </BrowserRouter>
     </div>
   )
---
+++ src/components/07-redux/coins-slice.tsx
@@ -0,0 +1,22 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type CoinModel from "../../models/CoinModel";
+
+interface CoinsState {
+    coins: CoinModel[]
+}
+
+const initialState: CoinsState = {
+    coins: []
+}
+
+const coinsSlice = createSlice({
+    name: 'coins',
+    initialState,
+    reducers: {
+        populate: (state, action: PayloadAction<CoinModel[]>) => {
+            state.coins = action.payload
+        }
+    }
+})
+export const { populate } = coinsSlice.actions
+export default coinsSlice.reducer
